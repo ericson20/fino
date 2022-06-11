3120c9e95936253ac80008fa2d7b31d2e921de32
ericson20 <ericson_092@hotmail.com>
2022-06-11 08:52:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 var whitelist = ['https://facturavirtual.pe']
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'build')));

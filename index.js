872439e97baffce41798e43cf3c25402adaaaec4
ericson20 <ericson_092@hotmail.com>
2022-06-11 08:46:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ require("dotenv").config();
 const path = require('path');
 // set up express
 
+var whitelist = ['https://facturavirtual.pe'] 
+
 const app = express();
 app.use(express.json());
 app.use(cors());
-var whitelist = ['https://facturavirtual.pe'] 
+
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
